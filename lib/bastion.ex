dc7fbb220c06a53d8c03d1d7f9825e7b620cbc8f
neodevelop <juan@makingdevs.com>
2016-07-12 20:36:33 -0500

    Adding Application, Router and Server from Cowboy

---
+++ lib/bastion.ex
@@ -0,0 +1,14 @@
+defmodule Bastion do
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      worker(Bastion.Server, [System.get_env('PORT')])
+    ]
+
+    opts = [strategy: :one_for_one, name: Bastion.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ lib/bastion/server.ex
@@ -0,0 +1,10 @@
+defmodule Bastion.Server do
+  @default_port 4000
+
+  def start_link(nil), do: start_server(@default_port)
+  def start_link(port), do: port |> String.to_integer() |> start_server()
+
+  defp start_server(port) do
+    Plug.Adapters.Cowboy.http Bastion.Router, [], port: port
+  end
+end
